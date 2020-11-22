5990c20e25f4372f41b8f72f69fc894087acf8c6
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-21 19:05:44 -0800

    Changed to just displaying 5 events

--- client/src/components/meteroids/meteroids.js
+++ client/src/components/meteroids/meteroids.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-
 class Meteroid extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      apiKey: 'goldapi-g8eiukhi5pe0t-io',
+      apiKey: 'Xb92XHUJvws2bncI1waLvjbgwiVnIiukwsFsRIXt',
       apiResults: [],
       todayDate: new Date().toISOString().slice(0,10),
-
 
     }
   }
@@ -23,21 +21,20 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.state.todayDate}&end_date=${this.state.todayDate}&api_key=Xb92XHUJvws2bncI1waLvjbgwiVnIiukwsFsRIXt`, requestOptions)
+fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.state.todayDate}&end_date=${this.state.todayDate}&api_key=${this.state.apiKey}`, requestOptions)
   .then(response => response.text())
   .then(result => this.setState({apiResults: JSON.parse(result)}))
   .then(result => {
-    var currentdate = this.state.apiResults.near_earth_objects[this.state.todayDate.toString()]
+  var apiData = this.state.apiResults.near_earth_objects[this.state.todayDate.toString()]
 
 for (var i = 0 ; i < 5; i++) {
-  console.log(currentdate[i])
-}
-
+let finalData =  []
+finalData.push(apiData[i])
+  console.log(finalData)
+  this.setState({
+    apiResults: finalData
   })
-  .then(result => {
-    this.setState({
-      apiResults: result.near_earth_objects
-    })
+}
   })
   .catch(error => console.log('error', error));
   }
